03a181387016393c4f7efaa3aae2a87899939910
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-17 15:34:33 -0500

    Initial Commit

---
+++ CTCI-CH1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CTCI_CH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
